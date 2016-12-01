52fc52370caf59720400aa7ad42de99c3837a15e
ramin <raminrajy@yahoo.com>
2016-12-01 09:43:38 +0100

    Tärningarnas css, html fil dice js

---
+++ dice/dice.js
@@ -0,0 +1,48 @@
+         
+   <div class="box">
+    <button id="roll"><p>Funky Yatzy</p></button>
+        <div class="second-face">
+          <span class="pip"></span>
+          <span class="pip"></span>
+        </div>
+        <div class="third-face">
+          <span class="pip"></span>
+          <span class="pip"></span>
+          <span class="pip"></span>
+        </div>
+        <div class="fourth-face">
+          <div class="column">
+            <span class="pip"></span>
+            <span class="pip"></span>
+          </div>
+          <div class="column">
+            <span class="pip"></span>
+            <span class="pip"></span>
+          </div>
+        </div>
+        <div class="fifth-face">
+          <div class="column">
+            <span class="pip"></span>
+            <span class="pip"></span>
+          </div>
+          <div class="column">
+            <span class="pip"></span>
+          </div>
+          <div class="column">
+            <span class="pip"></span>
+            <span class="pip"></span>
+          </div>
+        </div>
+        <div class="sixth-face">
+          <div class="column">
+            <span class="pip"></span>
+            <span class="pip"></span>
+            <span class="pip"></span>
+          </div>
+          <div class="column">
+            <span class="pip"></span>
+            <span class="pip"></span>
+            <span class="pip"></span>
+          </div>
+        </div>
+        </div>
