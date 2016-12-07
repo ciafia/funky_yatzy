4147e60b830f32fdc2838115ca4043538a971e35
jens_jacobsen <knasen_jens@hotmail.com>
2016-12-07 11:26:08 +0100

    1-6 funktioner

---
+++ js/table.js
@@ -0,0 +1,66 @@
+function ones(){
+	var count = 0;
+	dices.forEach(function(dice){
+
+		if(dice.value === 1){
+			count++
+		}
+	})
+	var result = count * 1;
+	console.log(result);
+}
+function twoes(){
+	var count = 0;
+	dices.forEach(function(dice){
+
+		if(dice.value === 2){
+			count++
+		}
+	})
+	var result = count * 2;
+	console.log(result);
+}
+function threes(){
+	var count = 0;
+	dices.forEach(function(dice){
+
+		if(dice.value === 3){
+			count++
+		}
+	})
+	var result = count * 3;
+	console.log(result);
+}
+function fours(){
+	var count = 0;
+	dices.forEach(function(dice){
+
+		if(dice.value === 4){
+			count++
+		}
+	})
+	var result = count * 4;
+	console.log(result);
+}
+function fives(){
+	var count = 0;
+	dices.forEach(function(dice){
+
+		if(dice.value === 5){
+			count++
+		}
+	})
+	var result = count * 5;
+	console.log(result);
+}
+function sixes(){
+	var count = 0;
+	dices.forEach(function(dice){
+
+		if(dice.value === 6){
+			count++
+		}
+	})
+	var result = count * 6;
+	console.log(result);
+}
