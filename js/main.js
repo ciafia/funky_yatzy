e7a833210d6749f35e2e401e3829ca56f2be4a51
jens_jacobsen <knasen_jens@hotmail.com>
2016-12-07 13:14:14 +0100

    hold gränssnitt

--- js/main.js
+++ js/main.js
@@ -50,9 +50,11 @@ console.log(dices);
             
         if(dices[0].rollable == true){
             dices[0].rollable = false;
+            $(".dice1").append("<p>Hold</p>");
         }
         else{
             dices[0].rollable = true;
+            $(".dice1 p").remove();
         }    
             console.log(dices[0].rollable);
     });
@@ -60,9 +62,11 @@ console.log(dices);
 
         if(dices[1].rollable == true){
             dices[1].rollable = false;
+            $(".dice2").append("<p>Hold</p>");
         }
         else{
             dices[1].rollable = true;
+            $(".dice2 p").remove();
         }
             console.log(dices[1].rollable);
 
@@ -71,9 +75,11 @@ console.log(dices);
 
         if(dices[2].rollable == true){
             dices[2].rollable = false;
+            $(".dice3").append("<p>Hold</p>");
         }
         else{
             dices[2].rollable = true;
+            $(".dice3 p").remove();
         }
             console.log(dices[2].rollable);
 
@@ -82,9 +88,11 @@ console.log(dices);
 
         if(dices[3].rollable == true){
             dices[3].rollable = false;
+            $(".dice4").append("<p>Hold</p>");
         }
         else{
             dices[3].rollable = true;
+            $(".dice4 p").remove();
         }
             console.log(dices[3].rollable);
 
@@ -92,9 +100,11 @@ console.log(dices);
     $(".dice5").on("click", function() {
         if(dices[4].rollable == true){
             dices[4].rollable = false;
+            $(".dice5").append("<p>Hold</p>");
         }
         else{
             dices[4].rollable = true;
+            $(".dice5 p").remove();
         }
             console.log(dices[4].rollable);
 
